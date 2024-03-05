ee75613c0cde757713ddd8d8605808bed990fdf5
Vinod Pahuja <vinod.pahuja@gmail.com>
2024-03-05 17:48:06 +0530

    Initial import

---
+++ index.js
@@ -0,0 +1,2 @@
+
+initWorkspace({ toolbox: toolbox });
---
+++ js/toolbox.js
@@ -0,0 +1,15 @@
+
+const toolbox = 
+`<xml id="toolbox" style="display: none">
+  <category name="Container">
+	<block type="create_mesh"></block>
+  </category>
+  <category name="Shapes">
+	<block type="create_cube"></block>
+	<block type="create_sphere"></block>
+	<block type="create_cylinder"></block>
+  </category>
+  <category name="Materials">
+	<block type="create_material"></block>
+  </category>
+</xml>`;
